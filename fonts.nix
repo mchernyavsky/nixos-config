ba8b794358d6c49aac0602a668e7cd181dd2d08e
mchernyavsky <chemike47@gmail.com>
2019-01-19 18:40:18 +0300

    Update fonts

--- fonts.nix
+++ fonts.nix
@@ -6,13 +6,10 @@
     enableGhostscriptFonts = true;
 
     fonts = with pkgs;  [
-      fira-mono
       hack-font
-      dejavu_fonts
-      font-awesome-ttf
-      terminus_font
-      libertine
-      liberation_ttf_v2_binary
+      fira-mono
+      ubuntu_font_family
+      incosolata
     ];
   };
 }
